06bb3c96824af77b21c3b5cadfda35c7a1bf52e4
Stefano Verna <s.verna@datocms.com>
2023-02-28 00:15:14 +0100

    Fix

--- components/NavigationMenu.tsx
+++ components/NavigationMenu.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { ActiveLink } from './ActiveLink';
 import { WhatsappIcon } from './WhatsappIcon';
 
@@ -13,7 +14,7 @@ function MenuItem({
   return (
     <ActiveLink
       segment={segment}
-      className="decoration-[#f4cf58] decoration-4 underline-offset-4 hover:underline"
+      className="p-4 decoration-[#f4cf58] decoration-4 underline-offset-4 hover:underline"
       activeClassName="underline"
       href={href}
     >
@@ -24,19 +25,22 @@ function MenuItem({
 
 export function NavigationMenu() {
   return (
-    <div className="fixed flex z-10 items-center left-0 px-10 py-4 top-10 uppercase font-bold space-x-4 backdrop-brightness-150 bg-white/40">
-      <MenuItem segment={null} href="/">
-        Home
-      </MenuItem>
-      <MenuItem href="/about" segment="about">
-        About
-      </MenuItem>
-      <MenuItem href="/contact" segment="contact">
-        Contact
-      </MenuItem>
-      <div>
-        <WhatsappIcon />
+    <>
+      <div className="h-32 lg:hidden" />
+      <div className="fixed flex z-10 items-center left-0 px-2 lg:px-8 top-10 uppercase font-bold backdrop-brightness-150 bg-white/50">
+        <MenuItem segment={null} href="/">
+          Home
+        </MenuItem>
+        <MenuItem href="/about" segment="about">
+          About
+        </MenuItem>
+        <MenuItem href="/contact" segment="contact">
+          Contact
+        </MenuItem>
+        <Link href="https://wa.me/393491769335" className="p-4">
+          <WhatsappIcon />
+        </Link>
       </div>
-    </div>
+    </>
   );
 }
