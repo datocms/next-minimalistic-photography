206caf872d59478dd59a8a0dcddd00606462f42e
Stefano Verna <s.verna@datocms.com>
2023-02-28 15:48:45 +0100

    Fix

--- components/NavigationMenu.tsx
+++ components/NavigationMenu.tsx
@@ -4,16 +4,13 @@ import { WhatsappIcon } from './WhatsappIcon';
 
 function MenuItem({
   href,
-  segment,
   children,
 }: {
   href: string;
-  segment: string | null;
   children: React.ReactNode;
 }) {
   return (
     <ActiveLink
-      segment={segment}
       className="p-4 decoration-accent decoration-4 underline-offset-4 hover:underline hover:decoration-accent/25"
       activeClassName="underline hover:decoration-accent"
       href={href}
@@ -28,15 +25,9 @@ export function NavigationMenu() {
     <>
       <div className="h-32 xl:hidden" />
       <div className="fixed flex z-10 items-center left-0 px-2 xl:px-8 top-10 uppercase font-bold backdrop-brightness-150 text-black bg-white/50">
-        <MenuItem segment={null} href="/">
-          Home
-        </MenuItem>
-        <MenuItem href="/about" segment="about">
-          About
-        </MenuItem>
-        <MenuItem href="/contact" segment="contact">
-          Contact
-        </MenuItem>
+        <MenuItem href="/">Home</MenuItem>
+        <MenuItem href="/about">About</MenuItem>
+        <MenuItem href="/contact">Contact</MenuItem>
         <Link href="https://wa.me/message/22R7RKDSMVMYC1" className="p-4">
           <WhatsappIcon className="w-[1em]" />
         </Link>
